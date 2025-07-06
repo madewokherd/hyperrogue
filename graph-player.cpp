b034a32071349a882be8bb563b03fc388ce26e0d
Esme Povirk <madewokherd@gmail.com>
2025-07-06 02:31:01 +0000

    Make thorns weapon configurable.

--- graph-player.cpp
+++ graph-player.cpp
@@ -480,6 +480,8 @@ EX void drawPlayer_humanoid(eMonster m, cell *where, const shiftmatrix& V, color
   else if(items[itCurseWeakness]) {
     /* no weapon shown */
     }
+  else if(bow::weapon == bow::wThorns)
+    queuepoly(VWPN, cgi.shHedgehogBladePlayer, items[itOrbDiscord] ? watercolor(0) : 0x00FF00FF);
   else if(!shmup::on)
     queuepoly(VWPN, cs.charid >= 2 ? cgi.shSabre : cgi.shPSword, fc(314, cs.swordcolor, 3)); // 3 not colored
   else if(shmup::curtime >= shmup::getPlayer()->nextshot)
@@ -657,15 +659,17 @@ EX void drawMimic(eMonster m, cell *where, const shiftmatrix& V, color_t col, do
       }
     else if(!shmup::on) {
       bool emp = items[itOrbEmpathy] && m != moShadow;
-      if(items[itOrbThorns] && emp)
+      if((items[itOrbThorns] && emp) || bow::weapon == bow::wThorns)
         queuepoly(VBODY * VBS, cgi.shHedgehogBladePlayer, darkena(col, 0, 0x40));
       if(items[itOrbSide1] && !shmup::on)
         queuepoly(VBODY * VBS * spin(-15._deg), cs.charid >= 2 ? cgi.shSabre : cgi.shPSword, darkena(col, 0, 0x40));
       if(items[itOrbSide3] && emp)
         queuepoly(VBODY * VBS, (cs.charid&1) ? cgi.shFerocityF : cgi.shFerocityM, darkena(col, 0, 0x40));
 
-      shiftmatrix VWPN = cs.lefthanded ? VBODY * VBS * lmirror() : VBODY * VBS;
-      queuepoly(VWPN, (cs.charid >= 2 ? cgi.shSabre : cgi.shPSword), darkena(col, 0, 0XC0));
+      if (bow::weapon != bow::wThorns) {
+        shiftmatrix VWPN = cs.lefthanded ? VBODY * VBS * lmirror() : VBODY * VBS;
+        queuepoly(VWPN, (cs.charid >= 2 ? cgi.shSabre : cgi.shPSword), darkena(col, 0, 0XC0));
+        }
       }
     else if(!where || shmup::curtime >= shmup::getPlayer()->nextshot)
       queuepoly(VBODY * VBS, cgi.shPKnife, darkena(col, 0, 0XC0));
